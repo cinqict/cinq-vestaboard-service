978f46af0d4299796d2b3a89f34dca55e01e3ef9
Daniel Eijkelenboom <91720000+DEijkelenboom@users.noreply.github.com>
2026-06-16 09:06:20 +0200

    move getTextMessage function to service instead of controller

--- src/main/java/com/cinq/vestaboard/application/BoardService.java
+++ src/main/java/com/cinq/vestaboard/application/BoardService.java
@@ -19,6 +19,23 @@ public class BoardService {
         return vestaBoardGateway.getCurrentMessage().getCurrentMessage().getLayout();
     }
 
+    public String getCurrentTextMessage() {
+        GetCurrentMessageResponse currentMessage = vestaBoardGateway.getCurrentMessage();
+        String layout = currentMessage.getCurrentMessage().getLayout();
+
+        String stripped = layout.replaceAll("\\s", "").replaceAll("^\\[\\[|]]$", "");
+        String[] rows = stripped.split("],\\[");
+
+        StringBuilder sb = new StringBuilder();
+        for (String row : rows) {
+            for (String code : row.split(",")) {
+                sb.append(decodeCharacter(Integer.parseInt(code)));
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
     public String setMessage(SetMessageRequest request, boolean force) {
         VestaboardMessage vestaboardMessage = messageCreator.create(request.getType(), request.getParams());
         int[][] composedMessage = vestaBoardGateway.compose(vestaboardMessage);
@@ -40,4 +57,44 @@ public class BoardService {
         VestaboardMessage vestaboardMessage = messageCreator.create(request.getType(), request.getParams());
         return vestaBoardGateway.compose(vestaboardMessage);
     }
+
+
+    private static String decodeCharacter(int code) {
+        if (code == 0) return " ";
+        if (code >= 1 && code <= 26) return String.valueOf((char) ('A' + code - 1));
+        return switch (code) {
+            case 27 -> "1";
+            case 28 -> "2";
+            case 29 -> "3";
+            case 30 -> "4";
+            case 31 -> "5";
+            case 32 -> "6";
+            case 33 -> "7";
+            case 34 -> "8";
+            case 35 -> "9";
+            case 36 -> "0";
+            case 37 -> "!";
+            case 38 -> "@";
+            case 39 -> "#";
+            case 40 -> "$";
+            case 41 -> "(";
+            case 42 -> ")";
+            case 44 -> "-";
+            case 46 -> "+";
+            case 47 -> "&";
+            case 48 -> "=";
+            case 49 -> ";";
+            case 50 -> ":";
+            case 52 -> "'";
+            case 53 -> "\"";
+            case 54 -> "%";
+            case 55 -> ",";
+            case 56 -> ".";
+            case 59 -> "/";
+            case 60 -> "?";
+            case 62 -> "°";
+            default -> "{" + code + "}";
+        };
+    }
+
 }
